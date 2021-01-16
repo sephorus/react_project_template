d5147602e1b9636eabac1174eceab8d2012f3abd
Joel Ramos <ramosj3@wwu.edu>
2021-01-16 15:49:40 -0800

    complete react project template

---
+++ react_template/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>React Project Template</h1>
+      <h3>Created by Joel Ramos</h3>
+    </div>
+  );
+}
+
+export default App;
